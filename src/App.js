08cba617730ae3714a5509a6231713b70d63b085
youssef-99 <youssefwilliam970@gmail.com>
2024-11-26 00:08:42 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
 import Todo from "./components/Todo/Todo";
+import { Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <Link to={"/about"}>Go To About</Link>
+    </>
+  );
+}
+function About() {
+  return <h1>About page</h1>;
+}
+
+function Contact() {
+  return <h1>About page</h1>;
+}
 function App() {
   return (
     <div className="App">
-      <Todo />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </div>
   );
 }
